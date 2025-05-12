aa11bc4e706d56b258195841db2e7641b87d96bb
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-05-12 12:07:09 +0530

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -12,7 +12,9 @@ public class Board extends JPanel implements ActionListener {
     private final int random_position = 29;
     private int apple_x;
     private int apple_y;
-    protected Timer timer;
+    private Timer timer;
+
+    private boolean inGame = true;
 
     private final int all_dots = 900;
     private final int[] x = new int[all_dots];
@@ -31,6 +33,7 @@ public class Board extends JPanel implements ActionListener {
         addKeyListener(new TAdapter());
         setBackground(Color.BLACK);
         setFocusable(true);
+        setPreferredSize(new Dimension(300,300));
 
         loadImages();
         initGame();
@@ -53,7 +56,7 @@ public class Board extends JPanel implements ActionListener {
             x[i] += 50 - i * dot_size;
         }
         locateApple();
-        timer = new Timer(140,this);
+        timer = new Timer(100,this);
         timer.start();
     }
     public void locateApple(){
@@ -68,15 +71,27 @@ public class Board extends JPanel implements ActionListener {
         draw(g);
     }
     public void draw(Graphics g){
-        g.drawImage(apple,apple_x,apple_y,this);
-        for(int i=0;i<dots;i++){
-            if(i==0){
-                g.drawImage(head,x[i],y[i],this);
-            }else{
-                g.drawImage(dot,x[i],y[i],this);
+        if(inGame) {
+            g.drawImage(apple, apple_x, apple_y, this);
+            for (int i = 0; i < dots; i++) {
+                if (i == 0) {
+                    g.drawImage(head, x[i], y[i], this);
+                } else {
+                    g.drawImage(dot, x[i], y[i], this);
+                }
             }
+            Toolkit.getDefaultToolkit().sync();
         }
-        Toolkit.getDefaultToolkit().sync();
+        else{
+            gameOver(g);
+        }
+    }
+    public void gameOver(Graphics g){
+        String msg = "Game Over!!!";
+        Font font = new Font("San SERIF",Font.BOLD,20);
+        FontMetrics metrices = getFontMetrics(font);
+        g.setColor(Color.white);
+        g.drawString(msg,((300-metrices.stringWidth(msg))/2),300/2);
     }
     public void move(){
         for(int i=dots;i>0;i--){
@@ -96,8 +111,40 @@ public class Board extends JPanel implements ActionListener {
             y[0]+= dot_size;
         }
     }
+    public void checkApple(){
+        if((x[0]==apple_x) && (y[0]==apple_y)){
+            dots++;
+            locateApple();
+        }
+    }
+    public void checkCollision(){
+        for(int i=dots;i>0;i--){
+            if((i>4) && ((x[0]==x[i])&&(y[0]==y[i]))) {
+                inGame = false;
+            }
+        }
+        if(y[0]>=300){
+            inGame=false;
+        }
+        if(x[0]>=300){
+            inGame=false;
+        }
+        if(y[0]<0){
+            inGame=false;
+        }
+        if(x[0]<0){
+            inGame=false;
+        }
+        if(!inGame){
+            timer.stop();
+        }
+    }
     public void actionPerformed(ActionEvent ae){
-        move();
+        if(inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
         repaint();
     }
     public class TAdapter extends KeyAdapter{
